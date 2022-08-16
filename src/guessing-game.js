f3f10b48d825e2c24e5fa5d5617aa7abf8424d9b
Elena Kuznetsova <kuzn.elena@yahoo.com>
2022-08-16 21:46:31 +0300

    init: task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,45 @@
-class GuessingGame {
+class GuessingGame { 
+    min;
+    max;
+    low;
+    previousDigit;
     constructor() {}
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        this.previousDigit = Math.round((this.min + this.max) / 2);
+        return this.previousDigit;
     }
 
     lower() {
+        this.max = this.previousDigit;
+        this.low = true;
 
     }
 
     greater() {
 
+        this.min = this.previousDigit;
+        this.low = false;
     }
 }
 
 module.exports = GuessingGame;
+
+let game = new GuessingGame();
+game.setRange(1, 5000);
+let digit = Math.floor(Math.random() * 5000) + 1;
+console.log(`The number is ${digit}`);
+while(true) {
+    let guess = game.guess();
+    console.log(guess);
+    let gu = game.guess();
+    if(gu === digit) break;
+    if(gu < digit) game.greater();
+    else game.lower();
+}
